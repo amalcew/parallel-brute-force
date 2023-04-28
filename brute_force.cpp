53df97f1b58e7f79e968ee4819244ea157514170
Alexander Malcew <amalcew@protonmail.com>
2023-04-28 13:54:43 +0200

    flsh switch implemented

--- brute_force.cpp
+++ brute_force.cpp
@@ -2,20 +2,21 @@
 
 #define START 97 //33
 #define END 122 // 126
-#define LENGTH 4
+#define LENGTH 2
 
-std::string recursiveBrute(std::string cipher, bool verbose=true, std::string currStr="") {
+std::string recursiveBrute(std::string cipher, bool verbose=true, bool flsh=true, std::string currStr="") {
     std::string found;
     if (currStr.length() < LENGTH) {
         for (int asciiCode = START; asciiCode <= END; ++asciiCode) {
             std::string nextStr = currStr + char(asciiCode);
-            if (verbose) std::cout << "\r" << "current: " << nextStr << std::flush;
+            if (verbose && flsh) std::cout << "\r" << "current: " << nextStr << std::flush;
+            if (verbose && !flsh) std::cout << nextStr << std::endl;
             if (nextStr == cipher) {
                 if (verbose) std::cout << "\n" << std::endl;
                 return nextStr;
             }
-            found = recursiveBrute(cipher, verbose, nextStr);
-            if (found.length() > 0) {
+            found = recursiveBrute(cipher, verbose, flsh, nextStr);
+            if (!found.empty()) {
                 return found;
             }
         }
@@ -23,9 +24,9 @@ std::string recursiveBrute(std::string cipher, bool verbose=true, std::string cu
     return "";
 }
 
-void bruteForce(std::string cipher, bool verbose) {
-    std::string found = recursiveBrute(cipher=cipher, verbose=verbose);
-    if (found.length() > 0) {
+void bruteForce(std::string cipher, bool verbose, bool flsh) {
+    std::string found = recursiveBrute(cipher=cipher, verbose=verbose, flsh=flsh);
+    if (!found.empty()) {
         std::cout << "password found: " << found << std::endl;
     } else {
         std::cout << "password not found!" << std::endl;
@@ -35,6 +36,7 @@ void bruteForce(std::string cipher, bool verbose) {
 int main() {
     std::string plain = "hjkl";
     bool verbose = true;
-    bruteForce(plain, verbose);
+    bool flsh = false;
+    bruteForce(plain, verbose, flsh);
     return 0;
 }
